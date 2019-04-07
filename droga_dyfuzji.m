61d528e3567be46d7cc253c0f13f5088c4262c4c
Wallace92 <mateuszwallace@gmail.com>
2019-04-07 23:04:45 +0200

    Difusion Lenght ODE in Matlab

---
+++ droga_dyfuzji.m
@@ -0,0 +1,18 @@
+clear;
+clc;
+
+solinit = bvpinit(linspace(0,50,100),[10^14 10^13]);
+sol = bvp4c(@twoode,@twobc,solinit);
+x = linspace(0,10);
+y=deval(sol,x);
+%plot(x,y(1,:))
+pk = deval(sol, 6)
+p = 10^13+[10^14-10^13]*exp(-x / 6);
+figure
+subplot(1,2,1)
+pk2 = 10^13+[10^14-10^13]*exp(-6 / 6)
+plot(x,y(1,:), 6, pk(1,:), 'o', 'MarkerSize', 10)         
+title('Obliczona numerycznie p(L = 6 um)')
+subplot(1,2,2)       
+plot(x,p(1,:), 6, pk2(1,:), 'o', 'MarkerSize', 10)    
+title('Obliczona analitycznie p(L = 6 um')
