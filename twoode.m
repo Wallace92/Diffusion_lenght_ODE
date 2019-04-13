30a0d2a21de272e0ff6b195d96c475ec3280d838
Mateusz Glinkowski <48870815+Wallace92@users.noreply.github.com>
2019-04-13 16:34:31 +0200

    Update twoode.m

--- twoode.m
+++ twoode.m
@@ -1,4 +1,5 @@
+%the two ordinary differential equation expresed by left and right sides of equation y2^' = (y(1) / 36) - (10^13 / 36) 
 function dydx = twoode(x,y)
-dydx = [ y(2); (y(1) / 36) - (10^13 / 36) ];  % droga dyfuzji
+dydx = [ y(2); (y(1) / 36) - (10^13 / 36) ]; 
 end
 
