9d801fda3c4cc0695ade16eaece8f6487f2320f8
Mateusz Glinkowski <48870815+Wallace92@users.noreply.github.com>
2019-04-13 16:30:30 +0200

    Update droga_dyfuzji.m

--- droga_dyfuzji.m
+++ droga_dyfuzji.m
@@ -1,21 +1,33 @@
 clear;
 clc;
-% solinit - vector with initial guessed values of equation
+
 % bcfun - function with bounduary conditions 
 % odefun - function with columnar vector
 
+% solinit - vector with initial guessed values of equation
 solinit = bvpinit(linspace(0,50,100),[10^14 10^13]);
+
+%the bvp4c solution
 sol = bvp4c(@twoode,@twobc,solinit);
+
+%the x vector
 x = linspace(0,10);
+
+%the y vector with evaluation of solution at x point
 y=deval(sol,x);
-%plot(x,y(1,:))
+
+%evaluate the value at x = 6 and value of carrier concentration at this point
 pk = deval(sol, 6)
 p = 10^13+[10^14-10^13]*exp(-x / 6);
+
+%an subplot to compare the numerical and analitycal equation
 figure
 subplot(1,2,1)
+
+plot(x,y(1,:), 6, y(1,:), 'o', 'MarkerSize', 10)         
+title('Numerical at p(L = 6 um)')
+subplot(1,2,2) 
+%the analitycal equation
 pk2 = 10^13+[10^14-10^13]*exp(-6 / 6)
-plot(x,y(1,:), 6, pk(1,:), 'o', 'MarkerSize', 10)         
-title('Obliczona numerycznie p(L = 6 um)')
-subplot(1,2,2)       
 plot(x,p(1,:), 6, pk2(1,:), 'o', 'MarkerSize', 10)    
-title('Obliczona analitycznie p(L = 6 um')
+title('Analitical at p(L = 6 um')
