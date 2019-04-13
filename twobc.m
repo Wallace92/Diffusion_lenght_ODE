330e05d3bf6e38cde1688218c3f1d1709f03d43f
Mateusz Glinkowski <48870815+Wallace92@users.noreply.github.com>
2019-04-13 16:32:30 +0200

    Update twobc.m

--- twobc.m
+++ twobc.m
@@ -1,3 +1,4 @@
+% the boundary conctidions ya(1)-10^14 -> y[0] - 10^14 = 0 and yb(1) - 10^13 -> y[\infty] - 10^13 = 0
 function res = twobc(ya,yb)
 res = [ ya(1)-10^14; yb(1) - 10^13 ];  % droga dyfuzji
 end
